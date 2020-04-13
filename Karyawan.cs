d8e0e4e2da710f08b214e69c16ba353f9a454ac7
hilmanauliarahman <hilman.ra@students.amikom.ac.id>
2020-04-13 15:03:54 +0700

    hilman 2656 dibuat dgn vs 2008 dirumah

---
+++ Karyawan.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ResponsiPemrograman2656
+{
+    class Karyawan
+    {
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public double GajiBulanan { get; set; }
+        //public double Diskon { get; set; }
+
+        public Karyawan(int nik, string nama, double gajibulanan)
+        {
+            Nik = nik;
+            Nama = nama;
+            GajiBulanan = gajibulanan;
+            //Diskon = diskon;
+
+            if (GajiBulanan < 0)
+            {
+                Console.WriteLine("\nNominal tidak valid!, Gaji tidak boleh kurang dari 0 !!!");
+                GajiBulanan = 0;
+            }
+        }
+        public void GetDataKaryawan()
+        {
+            Console.WriteLine("\nNo. Nik/Nama           Gaji Bulanan : ");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("1.  {0} / {1}     {2}", Nik, Nama, GajiBulanan);
+        }
+        public void GetDataKaryawan2()
+        {
+            Console.WriteLine("2.  {0} / {1}     {2}", Nik, Nama, GajiBulanan);
+        }
+
+        public void GetDataDiskon()
+        {
+            double diskon = 0;
+            diskon = 0.1 * GajiBulanan+GajiBulanan;
+            GajiBulanan += Convert.ToInt32(diskon);
+            Console.WriteLine("\nAsyiiiik kenaikan gaji 10% !!!");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("1.  {0} / {1}     {2}", Nik, Nama, diskon);
+        }
+        public void GetDataDiskon2()
+        {
+            double diskon = 0;
+            diskon = 0.1 * GajiBulanan + GajiBulanan;
+            GajiBulanan += Convert.ToInt32(diskon);
+            Console.WriteLine("2.  {0} / {1}     {2}", Nik, Nama, diskon);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ResponsiPemrograman2656
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan(19112656, "Paijo", 3000000);
+            karyawan.GetDataKaryawan();
+
+            Karyawan karyawan2 = new Karyawan(19112658, "Jono", 2000000);
+            karyawan.GetDataKaryawan2();
+
+
+            Karyawan GetDataDiskon = new Karyawan(191122, "Paijo", 0);
+            karyawan.GetDataDiskon();
+
+            Karyawan GetDataDiskon2 = new Karyawan(191213, "Jono", 0);
+            karyawan.GetDataDiskon2();
+
+            Console.ReadKey();
+        }
+    }
+}
